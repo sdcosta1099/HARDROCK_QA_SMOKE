1d6a14f874185e8d725c4ff2a3f21b91bcb0256e
Sheldon.Dcosta <sheldon..dcosta@shrss.com>
2024-01-17 08:19:29 -0500

    2024_1

--- tests/RET_1286.spec.js
+++ tests/RET_1286.spec.js
@@ -3,17 +3,14 @@ test.setTimeout(60000);
 import { commonSteps } from '../pages/commonSteps';
 import { playerAction } from '../pages/playerAction';
 
-<<<<<<< HEAD
 test('RET_1286_Validate PDP Category Breadcrumb', async ({ page }) => {
-=======
-test('PDP Category Breadcrumb', async ({ page }) => {
->>>>>>> 22744f9a47181f9ea5286447d0f8d61716ef0e7a
+
     const common = new commonSteps(page)
     const pa = new playerAction(page)
     await common.goToHALOShop();
     await common.closeHolidayDealsPopup()
 
-<<<<<<< HEAD
+
   await pa.hoveroncategoryWOMEN()
   await pa.clickonsubcategoryJOGGERSETS()
   await pa.validatePDP_Path_Home_Category_SubCategory()
@@ -21,17 +18,13 @@ test('PDP Category Breadcrumb', async ({ page }) => {
   await pa.validate_WOMEN_Category_PDP_PATH_is_clickable()
   await pa.validate_JOGGERSET_subcategory_is_clickable()
 
-});
 
-
-=======
-    await pa.clickOnCategoryPINS()
-    await pa.clickOnSubCategory3DPINS()
-    await pa.validatehomeCategorySubcatogeryPathDisplayed()
-    await pa.clickOnProductPINS()
-    await pa.validateProductDetailPage()
+    //await pa.clickOnCategoryPINS()
+    //await pa.clickOnSubCategory3DPINS()
+    //await pa.validatehomeCategorySubcatogeryPathDisplayed()
+    //await pa.clickOnProductPINS()
+    //await pa.validateProductDetailPage()
     
 
     
 });
->>>>>>> 22744f9a47181f9ea5286447d0f8d61716ef0e7a
