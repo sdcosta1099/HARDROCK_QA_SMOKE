537e4bb5ad17e94f2aaa5fe2fb3a6aa3aed874d6
Sheldon.Dcosta <sheldon..dcosta@shrss.com>
2024-01-17 07:10:29 -0500

    COMMIT_17

--- pages/commonSteps.js
+++ pages/commonSteps.js
@@ -13,13 +13,12 @@ exports.commonSteps = class commonSteps{
     
     
     async goToHALOShop(){
-<<<<<<< HEAD
+
         //await this.page.goto('https://storefront:Rock$h0p@staging-na01-hardrock.demandware.net/');
         await this.page.goto('https://storefront:Rock$h0p@dev.shop.hardrock.com/');
         await expect(this.page).toHaveTitle('Hard Rock - Online Rock Shop | Online Hard Rock Store ');
         //await expect(this.page).toHaveURL('https://staging-na01-hardrock.demandware.net/');
-        await expect(this.page).toHaveURL('https://dev.shop.hardrock.com/');
-=======
+        await expect(this.page).toHaveURL('https://dev.shop.hardrock.com/')
         await this.page.goto('https://storefront:Rock$h0p@staging-na01-hardrock.demandware.net/');
 
         //await this.page.goto('https://storefront:Rock$h0p@dev.shop.hardrock.com/');
