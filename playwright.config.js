bdd511f6dc96026e84af94492df819f8399cfd41
Sheldon.Dcosta <sheldon..dcosta@shrss.com>
2024-09-13 14:23:41 -0400

    test

--- playwright.config.js
+++ playwright.config.js
@@ -88,7 +88,7 @@ module.exports = defineConfig({
        name: 'chromium',
        use: { //...devices['Desktop Chrome'],
               channel: 'chrome',
-              headless: false,
+              headless: true,
               viewport: null
           },
      },
