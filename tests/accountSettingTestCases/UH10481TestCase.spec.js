84555b69fd255bea5b9bb411df9cb44341fcbc7d
shrssrene <milou.rene@shrss.com>
2025-10-29 13:51:15 -0400

    Oct2 Push

--- tests/accountSettingTestCases/UH10481TestCase.spec.js
+++ tests/accountSettingTestCases/UH10481TestCase.spec.js
@@ -3,7 +3,7 @@ import{globalFUnctionSteps}from '../../testSteps/gobalFunctionsSteps'
 import{playerSearchSteps}from '../../testSteps/playerSearchSteps'
 const testData= JSON.parse(JSON.stringify(require("../../testData.json")))
 
-test('UH10480 HALO | Online Account Settings | Enable an Online Account', async ({ page }) => {
+test('UH10481 HALO | Online Account Settings | Enable an Online Account', async ({ page }) => {
      
 
     const gblFuntSteps = new globalFUnctionSteps(page)
